55d63eeaaa55ccada6f3bf4b53780b50ecea581f
Ariel Tubaltsev <tubaltzev@gmail.com>
2015-12-30 11:08:24 -0800

    hierarchical configuration

--- config/initializers/agglue.rb
+++ config/initializers/agglue.rb
@@ -1,16 +1,16 @@
-puts "loading config"
 APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
+puts "config #{APP_CONFIG}"
 
-puts "loading redis"
+puts "loading redis #{APP_CONFIG['cache']['ip']}:#{APP_CONFIG['cache']['port']}"
 $redis = Redis.new(:host => APP_CONFIG['redis'], :port => 6379)
 
-puts "loading instance"
+puts "loading instance #{APP_CONFIG['instance']}"
 $instance = APP_CONFIG['instance']
 $instance ||= 'Unknown'
 
 # metrics
 require "nunes"
-statsd = Statsd.new APP_CONFIG['metric'], 8125
+statsd = Statsd.new APP_CONFIG['metrics']['ip'], APP_CONFIG['metrics']['port']
 Nunes.subscribe(statsd)
 
 Rails.logger = ActFluentLoggerRails::Logger.
